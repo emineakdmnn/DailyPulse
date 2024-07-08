095a31f8886c291a7492ae8d4d19118cf1a481b4
Emine Akduman <eakdmn@gmail.com>
2024-07-08 22:41:37 +0300

    ios UI completed

---
+++ iosApp/iosApp/SwiftUIView.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftUIView.swift
+//  iosApp
+//
+//  Created by Emine Akduman on 8.07.2024.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
